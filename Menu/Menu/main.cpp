a3bf6ce0c656d1eba9a74ed02d1467f03edb2732
Constantin - Adrian Lulea <adrian.lulea@gmail.com>
2017-03-26 18:43:19 +0300

    Initial Commit First Hello World from Xcode :-)

---
+++ Menu/Menu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Menu
+//
+//  Created by Constantin - Adrian Lulea on 3/26/17.
+//  Copyright © 2017 Constantin - Adrian Lulea. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
